c7d2d62edd613125c4e84f6d6a4fdfd70706269b
dzld00 <38360844+dzld00@users.noreply.github.com>
2024-06-19 22:59:11 -0700

    Update utils_retrieval.py

--- utils_retrieval.py
+++ utils_retrieval.py
@@ -36,15 +36,12 @@ def image_embeddings(image_paths, model, transform):
     embeddings = []
 
     for image_path in image_paths:
-        # Load and preprocess the image
         img = Image.open(image_path).convert('RGB')
         img_tensor = transform(img).unsqueeze(0)
         img_tensor = img_tensor.to('cuda' if torch.cuda.is_available() else 'cpu')
 
-        # Get the image embedding
         with torch.no_grad():
             embedding = model(img_tensor)
-
         embeddings.append(embedding)
 
     # Stack the embeddings
@@ -76,8 +73,6 @@ def cosine_similarity(vecA, vecB):
     return dot_product / (normA * normB)
 
 
-import numpy as np
-
 def gaussian_kernel(x, y, bandwidth):
     return np.exp(-np.linalg.norm(x - y)**2 / (bandwidth**2))
 
@@ -92,47 +87,30 @@ def MMD(X, Y, kernel='rbf', **kwargs):
     n = X.shape[0]
     m = Y.shape[0]
 
-    # Compute pairwise kernel values
     K_YY = pairwise_kernels(Y, Y, metric=kernel, **kwargs)
     np.fill_diagonal(K_YY, 0)
-    
     K_XX = pairwise_kernels(X, X, metric=kernel, **kwargs)
     np.fill_diagonal(K_XX, 0)
     K_XY = pairwise_kernels(X, Y, metric=kernel, **kwargs)
-    #return (np.sum(K_XX) / (n * (n - 1)) + np.sum(K_YY) / (m * (m - 1)) - 2 * K_XY.mean()) 
-    #return (np.sum(K_XX) / (n * (n - 1)) + np.sum(K_YY) / (m * (m - 1)) - 2 * np.sum(K_XY)/(m*n)) 
+
     kxx = np.sum(K_XX) / (n * (n - 1))
     kyy = np.sum(K_YY) / (m * (m - 1))
     kxy = np.sum(K_XY)/(m*n)
+    
     return kxx + kyy - 2*kxy, kxx, kyy, kxy
         
 
 def compute_wasserstein(X, Y):
-    # Compute pairwise distance matrices for X and Y
     Mxx = ot.dist(X, X)
     Myy = ot.dist(Y, Y)
     Mxy = ot.dist(X, Y)
     
-    # Compute marginals
     px = np.ones(X.shape[0]) / X.shape[0]
     py = np.ones(Y.shape[0]) / Y.shape[0]
 
-    # Compute Wasserstein distance
     wasserstein_distance = ot.emd2(px, py, Mxy)
     
     return wasserstein_distance
-
-
-def estimate_gamma(X, Y):
-    # Compute pairwise distances between all pairs of samples
-    distances = pairwise_distances(np.vstack((X, Y)), metric="euclidean")
-    
-    # Compute the median of the pairwise distances
-    median_distance = np.median(distances)
-    
-    # Return the inverse of the median distance as gamma
-    gamma = 1.0 / median_distance**2
-    return gamma
 
 
 def compute_MMD(query_embedding, other_embeddings, kernel='rbf', **kwargs):
@@ -146,118 +124,6 @@ def compute_MMD(query_embedding, other_embeddings, kernel='rbf', **kwargs):
     K_QO = pairwise_kernels(query_embedding, other_embeddings, metric=kernel, **kwargs)
 
     return K_QQ.mean() + (np.sum(K_OO) / (m * (m - 1))) - 2 * K_QO.mean()
-
-
-def twonn_dimension(X):
-    """
-    Estimate intrinsic dimensionality using the Two Nearest Neighbors (TWONN) method.
-    
-    Parameters:
-    - X: numpy array of shape (n_samples, n_features) - the data embeddings
-    
-    Returns:
-    - Estimated intrinsic dimension
-    """
-    
-    # Compute pairwise distances
-    distances = euclidean_distances(X)
-    
-    # Set diagonal to high value to exclude self-distance
-    np.fill_diagonal(distances, np.inf)
-    
-    # Sort each row and pick the first and second smallest values (smallest being the nearest neighbor)
-    r1 = np.sort(distances)[:, 0]
-    r2 = np.sort(distances)[:, 1]
-    
-    # Compute average ratio
-    rho_avg = np.mean(r1 / r2)
-    
-    # Estimate intrinsic dimension
-    D = 2 * np.log(2) / -np.log(rho_avg)
-    
-    return D
-
-
-def mds_dimension(embeddings):
-    batch_size = embeddings.shape[0] 
-
-    D = pairwise_distances(embeddings)   
-    D = D / np.amax(D)
-    
-    l_sorted = cmdscale(D) 
-    #l_sorted = eigen_mds(D)        
-    
-    # Calculate k based on number of large eigenvalues
-    k = next(x[0] for x in enumerate(l_sorted) if x[1] < 0.01 * l_sorted[0])  
-    
-    return k
-
-
-def cmdscale(D):
-    """                                                                                       
-    Classical multidimensional scaling (MDS)                                                  
-                                                                                               
-    Parameters                                                                                
-    ----------                                                                                
-    D : (n, n) array                                                                          
-        Symmetric distance matrix.                                                            
-                                                                                               
-    Returns                                                                                   
-    -------                                                                                   
-    Y : (n, p) array                                                                          
-        Configuration matrix. Each column represents a dimension. Only the                    
-        p dimensions corresponding to positive eigenvalues of B are returned.                 
-        Note that each dimension is only determined up to an overall sign,                    
-        corresponding to a reflection.                                                        
-                                                                                               
-    e : (n,) array                                                                            
-        Eigenvalues of B.                                                                     
-                                                                                               
-    """
-    
-    # Number of points                                                                        
-    n = len(D)
- 
-    # Centering matrix                                                                        
-    H = np.eye(n) - np.ones((n, n))/n
- 
-    # YY^T                                                                                    
-    B = -H.dot(D**2).dot(H)/2
- 
-    # Diagonalize                                                                             
-    evals, evecs = np.linalg.eigh(B)
- 
-    # Sort by eigenvalue in descending order                                                  
-    idx   = np.argsort(evals)[::-1]
-    evals = evals[idx]
-    evecs = evecs[:,idx]
- 
-    # Compute the coordinates using positive-eigenvalued components only                      
-#     w, = np.where(evals > 0)
-#     L  = np.diag(np.sqrt(evals[w]))
-#     V  = evecs[:,w]
-#     Y  = V.dot(L)
- 
-    return np.sort(evals)[::-1]
-
-
-def eigen_mds(pd):   
-    mds = MDS(n_components=len(pd), dissimilarity='precomputed')
-    pts = mds.fit_transform(pd)
-
-    _,l_sorted,_ = np.linalg.svd(pts)
-    
-    return l_sorted
-
-
-def BC_dist(mu1, cov1, mu2, cov2):
-    epsilon = 1e-5
-    cov1 += epsilon * np.eye(cov1.shape[0])
-    cov2 += epsilon * np.eye(cov2.shape[0])
-    mean_cov = 0.5 * (cov1 + cov2)
-    d = ((mu1 - mu2).T @ np.linalg.inv(mean_cov) @ (mu1 - mu2) / 8.).reshape(-1)[0]\
-            + np.log(np.linalg.det(mean_cov)/(np.linalg.det(cov1) * np.linalg.det(cov2))**0.5) / 2.
-    return -np.exp(-d)
 
 
 def permutation_test_mmd(x, y, kernel, degree=None, num_permutations=1000):
@@ -289,5 +155,4 @@ def permutation_test_mmd(x, y, kernel, degree=None, num_permutations=1000):
     p_value = np.mean(mmd_observed < mmd_permuted_values)
     
     return p_value
-
 
